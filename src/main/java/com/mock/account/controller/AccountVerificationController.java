4d0de30f46fd298bf7139f4879d0290552cd9a35
NILOHIT JHA <jhanilohit9286@gmail.com>
2021-08-24 09:39:21 -0500

    Test commit
    Updated Response Entity to return Json object

--- src/main/java/com/mock/account/controller/AccountVerificationController.java
+++ src/main/java/com/mock/account/controller/AccountVerificationController.java
@@ -2,8 +2,9 @@ package com.mock.account.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
+import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mock.account.model.Company;
+import com.mock.account.model.Score;
 
 @RestController
 public class AccountVerificationController {
 	
 	static Collection<String> accountlist = new ArrayList<String>();
+	private static final Gson gson = new Gson();
 	
 	@GetMapping(value="/account/v1/verify/{accountno}")
 	public ResponseEntity<String> verifyAccount(@PathVariable("accountno") String accountno)
@@ -35,17 +38,21 @@ public class AccountVerificationController {
 		
 	}
 	
-	@PostMapping(value="/account/v1/calculate/score")
-	public ResponseEntity<String> calculateScore(@RequestBody Company reqcompany)
+	@PostMapping(value="/account/v1/calculate/score", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Score> calculateScore(@RequestBody Company reqcompany)
 	{
-		
+		Score score = new Score();
 		if(reqcompany.getQuoteAmount() > 75000)
-			return new ResponseEntity<String>("45", HttpStatus.OK);
+			score.setVal(45);
 		if(reqcompany.getQuoteAmount() >= 50000 && reqcompany.getQuoteAmount() <= 75000 )
-			return new ResponseEntity<String>("65", HttpStatus.OK);
-		//if(reqcompany.getQuoteAmount() < 50000 )
-		return new ResponseEntity<String>("85", HttpStatus.OK);
+			score.setVal(65);
+		else
+			score.setVal(85);
+		
+		//return ResponseEntity.ok(gson.toJson("This is a String"));
 		//return new ResponseEntity<String>("Declined" , HttpStatus.NOT_FOUND);
+		 
+		 return new ResponseEntity<Score>(score, HttpStatus.OK);
 		
 	}
 	
---
+++ src/main/java/com/mock/account/model/Score.java
@@ -0,0 +1,15 @@
+package com.mock.account.model;
+
+public class Score {
+	
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+
+	private int val;
+
+}
